843746f4eb6bcaf8824b1810ea42d5a74a1ae67f
mavanherpen <m.a.vanherpen@gmail.com>
2024-11-29 09:43:39 +0100

    Add word count to the bot

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
-        
+    
+    words = file_contents.split()
+    sum_words = 0
+
+    for word in words:
+        sum_words += 1
+    
+    print("The number of words in this book =",sum_words)
+
 main()
