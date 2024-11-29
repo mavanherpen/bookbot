541aa2dab8701368f7b4d5ae12b77f6c9d03a368
mavanherpen <m.a.vanherpen@gmail.com>
2024-11-29 14:34:44 +0100

    add printed report with character count

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ def main():
     text = book_text(location)
     sum_words = get_sum_words(text)
     character_dict = get_character_count(text)
+    character_dict_sorted = dictionary_to_sorted_list(character_dict)
+    print("-- Report of", location,"--")
     print("The number of words in this book =",sum_words)
-    print(character_dict)
+    print()
+
+    for character in character_dict_sorted:
+        if character["char"].isalpha() == True:
+            print("The '",character["char"],"' character was found",character["num"],"times")
+        continue
+
+    print("-- End of report --")
 
 def book_text(location):    
     with open(location) as f:
@@ -27,5 +36,15 @@ def get_character_count(text):
         else:
             character_dict[character] = 1
     return character_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def dictionary_to_sorted_list(character_dict):
+    character_dict_sorted = []
+    for character in character_dict:
+        character_dict_sorted.append({"char": character, "num": character_dict[character]})
+    character_dict_sorted.sort(reverse=True, key=sort_on)
+    return character_dict_sorted
 
 main()
