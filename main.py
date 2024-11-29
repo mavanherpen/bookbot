a9f5f8b2e5281f4997de3329cc6dd581b3ce1c6e
mavanherpen <m.a.vanherpen@gmail.com>
2024-11-29 11:20:01 +0100

    Add character count to the bot

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-    
-    words = file_contents.split()
-    sum_words = 0
+    location = "books/frankenstein.txt"
+    text = book_text(location)
+    sum_words = get_sum_words(text)
+    character_dict = get_character_count(text)
+    print("The number of words in this book =",sum_words)
+    print(character_dict)
 
+def book_text(location):    
+    with open(location) as f:
+        return f.read()
+
+def get_sum_words(text):
+    sum_words = 0
+    words = text.split()
     for word in words:
         sum_words += 1
+    return sum_words
+
+def get_character_count(text):
+    lowered_text = text.lower()
+    character_dict = {}
     
-    print("The number of words in this book =",sum_words)
+    for character in lowered_text:
+        if character in character_dict:
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
+    return character_dict
 
 main()
